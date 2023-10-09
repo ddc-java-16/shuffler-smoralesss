f2449d125717fa9b77a7788926109f00597a1446
sam morales <smorales40@cnm.edu>
2023-10-09 12:01:34 -0600

    Replaced non-stream creation of test data arrays with streams.

--- src/test/java/edu/cnm/deepdive/shuffle/ShufflerTest.java
+++ src/test/java/edu/cnm/deepdive/shuffle/ShufflerTest.java
@@ -3,33 +3,53 @@ package edu.cnm.deepdive.shuffle;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.function.IntSupplier;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 
 class ShufflerTest {
 
   @Test
   void shuffle() {
-    int[] input = new int[1000];
+
     Random rng = new Random();
-    List<Integer> sortedValuesBefore = new ArrayList<>();
-    for (int i = 0; i < input.length; i++) {
-      input[i] = rng.nextInt();
-      sortedValuesBefore.add(input[i]);
-    }
-    Collections.sort(sortedValuesBefore);
+
+    int[] input = IntStream.generate(rng::nextInt)
+        .limit(1000)
+        .toArray();
+//    int[] input = new int[1000];
+//
+//    for (int i = 0; i < input.length; i++) {
+//      input[i] = rng.nextInt();
+//
+//    }
+
+    int[] sortedValuesBefore = copySorted(input);
+
+//    int[] sortedValuesBefore = Arrays.copyOf(input, input.length);
+//    Arrays.sort(sortedValuesBefore);
+
     Shuffler shuffler = new Shuffler();
     shuffler.shuffle(input);
-    List<Integer> sortedValuesAfter = new ArrayList<>();
-    for (int value : input) {
-      sortedValuesAfter.add(value);
-    }
-    Collections.sort(sortedValuesAfter);
-    assertEquals(sortedValuesBefore, sortedValuesAfter);
+
+    int[] sortedValuesAfter = copySorted(input);
+
+//    int[] sortedValuesAfter = Arrays.copyOf(input, input.length);
+//    Arrays.sort(sortedValuesAfter);
+
+    assertArrayEquals(sortedValuesBefore, sortedValuesAfter);
+  }
+
+  private static int[] copySorted(int[] input) {
+    return IntStream.of(input)
+        .sorted()
+        .toArray();
   }
 
 }
